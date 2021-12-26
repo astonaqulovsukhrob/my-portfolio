a5625676f5b39d28d5f9f0d16fb2dc746703dfe4
astonaqulovsukhrob <astonaqulovsukhro@gmail.com>
2021-12-26 11:13:28 +0300

    portfolio website

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const nav = document.querySelector(".nav");
 const mainIcon = document.querySelector(".main-header_icon");
 const body = document.querySelector("body");
 const sections = document.querySelectorAll("section[id]");
+const navLink = document.querySelectorAll(".nav-link");
 
 window.addEventListener("scroll", scrollActive);
 mainIcon.addEventListener("click", icon);
@@ -11,6 +12,10 @@ menuToggle.addEventListener("click", menu);
 
 // Scroll section active
 function scrollActive() {
+  // navbaar
+  menuToggle.classList.remove("active");
+  nav.classList.remove("active");
+
   if (window.scrollY > header.offsetHeight) header.classList.add("active");
   else header.classList.remove("active");
 
@@ -30,6 +35,30 @@ function scrollActive() {
     }
   });
 }
+
+// animation bar
+const main = document.querySelector("main");
+const progressBar = document.querySelector("#progress-bar");
+
+const animateProgressBar = () => {
+  let sceollDistance = -main.getBoundingClientRect().top;
+  let progressWidth =
+    (sceollDistance /
+      (main.getBoundingClientRect().height -
+        document.documentElement.clientHeight)) *
+    100;
+  const value = Math.floor(progressWidth);
+  progressBar.style.width = value + "%";
+};
+window.addEventListener("scroll", animateProgressBar);
+
+// navbar link
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
 // icon active
 function icon() {
   mainIcon.classList.toggle("active");
@@ -38,7 +67,6 @@ function icon() {
 
 // menu active
 function menu() {
-  header.classList.toggle("active");
   menuToggle.classList.toggle("active");
   nav.classList.toggle("active");
 }
@@ -49,7 +77,6 @@ const sr = ScrollReveal({
   distance: "60px",
   duration: 2000,
   delay: 200,
-  //     reset: true
 });
 
 sr.reveal(
@@ -59,5 +86,16 @@ sr.reveal(
 sr.reveal(".main-home_img, .about__subtitle, .about__text, .main-skills_img", {
   delay: 400,
 });
-sr.reveal(".main-skills_data,.work_img", { interval: 200 });
-// sr.reveal(".home__social-icon", { interval: 200 });
+sr.reveal(
+  ".main-skills_data,.work_img, .contact-input, .btn, .text, .footer-text, .footer-copy",
+  {
+    interval: 200,
+  }
+);
+sr.reveal(".icon", { interval: 200 });
+
+// Preserve 3d
+VanillaTilt.init(document.querySelectorAll(".icon li a "), {
+  max: 30,
+  speed: 400,
+});
